d267bbc76ad47ea8f2c3cbac46e22ee79d27f7ed
cgamcs <cesar.garciamcs@proton.me>
2025-09-08 13:01:43 -0600

    agregando inputs al formulario

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,7 +1,74 @@
+import { categories } from "../data/categories"
 
 function ExpenseForm() {
   return (
-    <div>ExpenseForm</div>
+    <form className="space-y-3">
+      <legend
+        className="uppercase text-center text-2xl font-black p-2 border-b-2 border-b-gray-400 "
+      >
+        Nuevo Gasto
+      </legend>
+
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="expenseName"
+          className="text-xl"
+        >
+          Nombre:
+        </label>
+
+        <input
+          id="expenseName"
+          type="text"
+          name="expenseName"
+          className="bg-gray-100 p-2 rounded-md"
+          placeholder="Agrega el nombre del gasto. Ej. Netflix"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="amount"
+          className="text-xl"
+        >
+          Cantidad:
+        </label>
+
+        <input
+          id="amount"
+          type="number"
+          name="amount"
+          className="bg-gray-100 p-2 rounded-md"
+          placeholder="Agrega la cantidad del gasto. Ej. 300"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="category"
+          className="text-xl"
+        >
+          Categoria:
+        </label>
+
+        <select
+          id="category"
+          name="category"
+          className="bg-gray-100 p-2 rounded-md"
+        >
+          <option value="">-- Seleccione --</option>
+          {categories.map(category => (
+            <option key={category.id} value={category.id}>{category.name}</option>
+          ))}
+        </select>
+      </div>
+
+      <input
+        type="submit"
+        className="bg-blue-600 cursor-pointer text-white font-bold rounded-lg w-full p-2 uppercase"
+        value="Registrar Gasto"
+      />
+    </form>
   )
 }
 
