4e32fc020420de030091b6013248c916a83b4f81
cgamcs <cesar.garciamcs@proton.me>
2025-09-10 11:37:29 -0600

    guardando gastos y presupuesto en localStorage

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -17,10 +17,20 @@ export type BudgetState = {
   editingId: Expense['id']
 }
 
+const initailBudget = () : number => {
+  const localStorageBudget = localStorage.getItem('budget')
+  return localStorageBudget ? +localStorageBudget : 0
+}
+
+const localStorageExpenses = () : Expense[] => {
+  const localStorageExpenses = localStorage.getItem('expenses')
+  return localStorageExpenses ? JSON.parse(localStorageExpenses) : []
+}
+
 export const initialState : BudgetState = {
-  budget: 0,
+  budget: initailBudget(),
   modal: false,
-  expenses: [],
+  expenses: localStorageExpenses(),
   editingId: ''
 }
 
